6e31da3212cc6bd0de2137ff1a9c8ffb955d7b45
Mustafa Eray <meraykeskin@gmail.com>
2022-10-31 15:08:31 +0300

    Add PlanActionBar component

---
+++ src/components/PlanActionBar.tsx
@@ -0,0 +1,5 @@
+import { PlansActionBarContainer } from "../utils/styles"
+
+export const PlansActionBar = (props: any) => {
+    return <PlansActionBarContainer>{props.children}</PlansActionBarContainer>
+}
